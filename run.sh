01156cf2362b6111b65e6235bf031d50562e1fa8
loferreira <loferreira.nuno@gmail.com>
2023-01-16 17:28:48 +0100

    mo

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 export DOCKER_PATH=$(which docker)
-docker-compose up --detach --remove-orphans --force-recreate
+docker build -t jenkins-blueocean:0.0.1-1 || true .
+docker network create jenkins || true 
+docker-compose up --detach --remove-orphans --force-recreate 
