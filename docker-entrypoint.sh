52f27eeb0120f20ec5ea428129144e5af3dce7aa
Nuno Ferreira <nuno.ferreira@bestsecret.com>
2023-01-24 11:34:31 +0100

    a

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-cp -R /tmp/.ssh /var/jenkins_home/.ssh
+cp -R /tmp/.ssh /var/jenkins_home
 chmod 700 /var/jenkins_home/.ssh
 chmod 644 /var/jenkins_home/.ssh/id_rsa.pub
 chmod 600 /var/jenkins_home/.ssh/id_rsa
