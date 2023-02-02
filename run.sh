33044cbae9bd1cd78ce5c09093a9dabca46a5c11
loferreira <loferreira.nuno@gmail.com>
2023-02-02 19:30:49 +0100

    a

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 export DOCKER_PATH=$(which docker)
 export DOCKER_COMPOSE_PATH=$(which docker-compose)
-export DOCKER_COMPOSE_VERSION=
 docker build -t jenkins-blueocean:0.0.1-1 --build-arg SSH_KNOWN_HOSTS="$(cat ~/.ssh/known_hosts)" --build-arg SSH_PRIVATE_KEY="$(cat ~/.ssh/id_rsa)" --build-arg SSH_PUBLIC_KEY="$(cat ~/.ssh/id_rsa.pub)" . || true 
 docker-compose up --detach --remove-orphans --force-recreate 
